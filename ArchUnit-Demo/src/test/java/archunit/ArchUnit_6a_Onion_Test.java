0fe6a41df0051b8b53f3dc7392d4343c7b8da1be
Thomas Much <github@snailshell.de>
2022-01-25 14:11:24 +0100

    Update onion arch tests

--- ArchUnit-Demo/src/test/java/archunit/ArchUnit_6a_Onion_Test.java
+++ ArchUnit-Demo/src/test/java/archunit/ArchUnit_6a_Onion_Test.java
@@ -2,28 +2,39 @@ package archunit;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchIgnore;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
 
 //@RunWith(ArchUnitRunner.class) // necessary for JUnit 4
-@AnalyzeClasses(packages = "com.muchsoft")
+@AnalyzeClasses(packages = "oniondemo")
 public class ArchUnit_6a_Onion_Test {
 
     @ArchTest
-    @ArchIgnore
     public void adhere_to_onion_architecture(JavaClasses importedClasses) {
 
         ArchRule rule = onionArchitecture()
-                .domainModels("com.muchsoft.myapp.domain.model..")
-                .domainServices("com.muchsoft.myapp.domain.service..")
-                .applicationServices("com.muchsoft.myapp.application..")
-                .adapter("DB", "com.muchsoft.myapp.adapter.persistence..")
-                .adapter("REST", "com.muchsoft.myapp.adapter.rest..")
-                .adapter("SOAP", "com.muchsoft.myapp.adapter.soap..")
-                .adapter("UI", "com.muchsoft.myapp.adapter.jsf..");
+                .domainModels("oniondemo.domain.model..")
+                .domainServices("oniondemo.domain.service..")
+                .applicationServices("oniondemo.application..")
+                .adapter("DB", "oniondemo.adapter.persistence..")
+                .adapter("Web", "oniondemo.adapter.web");
+//                .adapter("REST", "oniondemo.adapter.rest..")
+//                .adapter("SOAP", "oniondemo.adapter.soap..")
+//                .adapter("UI", "oniondemo.adapter.jsf..").;
+
+        rule.check(importedClasses);
+    }
+
+    @ArchTest
+    public void no_framework_dependencies_in_domain(JavaClasses importedClasses) {
+
+        ArchRule rule = noClasses()
+                .that().resideInAPackage("oniondemo.domain..")
+                .should().dependOnClassesThat().resideInAPackage("org.springframework..")
+                .because("our domain core should be independent of frameworks");
 
         rule.check(importedClasses);
     }
